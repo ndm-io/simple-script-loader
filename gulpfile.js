1c343c1fb943f58a21cfde0aacc3381f4ee30f31
Rob Atherton <robather@googlemail.com>
2016-02-16 12:13:19 +0000

    fixed min suffix

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
 
 gulp.task('scripts', function () {
     gulp.src('src/*.js')
         .pipe(uglify({mangle: false}))
+        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('dist'));
 });
 
